bf2b0ee0ab628e36dc86cc386e22029c4868882f
unknown <jvaleriom@vallegrande.edu.pe>
2018-05-26 10:39:02 -0500

    Terminamos el scrpt sobre eñl manejo de BD

--- Lab16/SQLQuery1.sql
+++ Lab16/SQLQuery1.sql
@@ -6,3 +6,35 @@ go
 select database_id, name 
 from sys.databases;
 go
+
+use HospitalHHH;
+
+--usar una base de datos--
+use AdventureWorks2016;
+
+select name from sys.tables
+
+use AdventureWorks2016
+select name from sys.views;
+
+--visualizar la informacion detallada de una base de datos--
+EXEC sp_helpdb AdventureWorks2016;
+
+-- Create a new database called 'DatabaseName'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'DatabaseName'
+)
+CREATE DATABASE MercadoVG
+GO
+
+--eliminar una BD---
+drop database HospitalHHH;
+
+--Eliminar una base de datos adventure works--
+drop database AdventureWorks2016;
